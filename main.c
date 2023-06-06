7da3f7675643f79bc7690f3ef26373292e1329b2
wesley <wesleyemidio99@hotmail.com>
2023-06-05 23:27:31 -0300

    Criação do Desinfileirar

--- main.c
+++ main.c
@@ -40,16 +40,35 @@ bool enfileirar(tfila * ppf, int valor){
         }
     }
 }
+
+bool desinfileirar(tfila * ppf, int * pegaValor){
+    tno * aux;
+    if(ppf->comeco == NULL){
+        return false;
+    }
+    else{
+        aux = ppf->comeco;
+        *pegaValor = aux->dado;
+        ppf->comeco = aux->prox;
+        free(aux);
+        return true;
+    }
+}
 int main() {
     tfila * pf;
     int pegaValor;
 
     criar(&pf);
+
     if(!enfileirar(&pf,1)){
         printf("Erro ao enfileirar\n");
     }
     if(!enfileirar(&pf,2)){
         printf("Erro ao enfileirar\n");
+    }
+
+    if(desinfileirar(&pf,&pegaValor)){
+        printf("retirado %d do inicio da Fila!\n",pegaValor);
     }
     return 0;
 }
