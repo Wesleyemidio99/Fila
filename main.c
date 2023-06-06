84ae4200fcb6f8708f83b106cb52665a4c0f920f
wesley <wesleyemidio99@hotmail.com>
2023-06-05 23:41:34 -0300

    Criação do Menu

--- main.c
+++ main.c
@@ -56,19 +56,48 @@ bool desinfileirar(tfila * ppf, int * pegaValor){
 }
 int main() {
     tfila * pf;
-    int pegaValor;
+    int pegaValor, op = 0, valor = 0;
 
     criar(&pf);
 
-    if(!enfileirar(&pf,1)){
-        printf("Erro ao enfileirar\n");
-    }
-    if(!enfileirar(&pf,2)){
-        printf("Erro ao enfileirar\n");
-    }
+    do{
+        printf("Digite 1 - Enfileira / 2 - Desinfileirar / -1 para sair\n");
+        scanf("%d",&op);
 
-    if(desinfileirar(&pf,&pegaValor)){
-        printf("retirado %d do inicio da Fila!\n",pegaValor);
-    }
+        switch(op){
+            case 1:
+                do{
+                    printf("entre com o valor para enfileirar ou -1 para sair\n");
+                    scanf("%d",&valor);
+
+                    if(valor == -1){
+                        printf("Saindo...\n");
+                    }
+                    else{
+                        if(!enfileirar(&pf,valor)){
+                            printf("Erro ao enfileirar\n");
+                        }
+                    }
+                }while(valor != -1);
+                break ;
+
+            case 2:
+                do{
+                    printf("Digite 1 para desinfileirar e -1 para sair\n");
+                    scanf("%d",&valor);
+
+                    if(valor == 1) {
+                        if (desinfileirar(&pf, &pegaValor)) {
+                            printf("retirado %d do inicio da Fila!\n", pegaValor);
+                        }
+                        else {
+                            printf("Saindo...\n");
+                        }
+                    }
+                }while(valor!= -1);
+                break;
+             }
+    }while(op!=-1);
+
     return 0;
 }
